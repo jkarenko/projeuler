bdcf38d2fe4713e583dc131bb3dfac257a32366e
jkarenko <jk@noo.se>
2014-05-22 02:05:37 +0300

    5 done

--- 5.rb
+++ 5.rb
@@ -1,2 +1,22 @@
 # https://projecteuler.net/problem=5
 
+number = 1
+found = false
+dividers = (1..20).to_a
+while true
+	dividers.each do |x|
+		if number % x != 0
+			break
+		elsif x == dividers[-1]
+			found = true
+		end
+	end
+	if found
+		break
+	end
+	if number % 1000 == 0
+		puts number
+	end
+	number += 1
+end
+puts number
