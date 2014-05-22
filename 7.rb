5ed0fa1aa871ef451676ac7a5ccc3d04139f42a4
jkarenko <jk@noo.se>
2014-05-22 03:14:50 +0300

    7 done

---
+++ 7.rb
@@ -0,0 +1,16 @@
+primes = [1,2]
+num = 3
+while primes.size <= 10001
+	is_prime = true
+	for i in 2..Math.sqrt(num).ceil
+		if num % i == 0
+			is_prime = false
+			break
+		end
+	end
+	if is_prime
+		primes.push(num)
+	end
+	num += 1
+end
+puts "#{primes.size-1}. #{primes[-1]}"
